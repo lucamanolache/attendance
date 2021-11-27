d61fe676e36b2b4dfa478763173b2c6768be48bb
Luca Manolache <luca1230@gmail.com>
2021-11-27 14:01:27 -0800

    Added test html file to be returned

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,25 @@
+use actix_files::NamedFile;
 use simple_logger::SimpleLogger;
 use log::*;
+use actix_web::{App, HttpRequest, HttpServer, get};
+
+#[get("/")]
+async fn index(_req: HttpRequest) -> Result<NamedFile, actix_web::Error> {
+    Ok(NamedFile::open("./index.html")?)
+}
 
 #[actix_web::main]
 async fn main() -> Result<(), actix_web::Error> {
     SimpleLogger::new().init().unwrap();
     trace!("Started logger");
 
-    Ok(())
+    HttpServer::new(move || {
+        App::new()
+            .service(index)
+    })
+        .bind("127.0.0.1:3030")?
+        .run()
+        .await?;
+
+    return Ok(());
 }
