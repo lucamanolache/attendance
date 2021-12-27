9cd1d8c0a1941dfcfa004c6b3ba93e32ba8e670b
Luca Manolache <luca1230@gmail.com>
2021-12-27 15:48:52 -0800

    Added login form

--- static/src/components/Main.js
+++ static/src/components/Main.js
@@ -1,20 +1,70 @@
 import Routes from "../routes";
-import React from "react";
+import React, {useState} from "react";
 import TextField from '@mui/material/TextField';
 import Box from '@mui/material/Box';
 
 function Main() {
+    const [text, setText] = useState('')
+    const [error, setError] = useState(false)
+    const isValid = (t) => {
+        let valid = /^\d+$/.test(t);
+        valid &= t.length === 8
+        valid |= t.length === 0
+        if (!valid) {
+            setError(true);
+            setText(t);
+            return false;
+        } else {
+            setError(false);
+            setText(t);
+            return true;
+        }
+    }
+    const handleKeyDown = (event) => {
+        event.preventDefault();
+        if (event.key === 'Enter') {
+            if (isValid(text)) {
+                login(text)
+            }
+            setText('')
+        }
+    }
+
+    const login = (text) => {
+        console.log("Requesting to add " + text);
+
+        const request_options = {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({ id: parseInt(text) })
+        }
+
+        // TODO: add error handling
+        fetch("http://127.0.0.1:3030/api/login", request_options)
+            .then(response => response.json())
+            .then(data => {
+                console.log(data)
+            })
+    }
+
     return (
         <React.Fragment>
             <Box
-                component="form"
+                component="TextField"
                 sx={{
-                    '& > :not(style)': { m: 1, width: '25ch' },
+                    '& .MuiTextField-root': { m: 1, width: '25ch' },
                 }}
                 noValidate
                 autoComplete="off"
+                onKeyUp={handleKeyDown}
             >
-                <TextField id="login-box" label="Student ID" variant="standard" />
+                <TextField
+                    id="login-box"
+                    label="Student ID"
+                    variant="standard"
+                    error={error}
+                    value={text}
+                    onChange={s => isValid(s.target.value)}/>
             </Box>
         </React.Fragment>
     );
