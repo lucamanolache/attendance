60041923f89045440c93474d7a599f0f35be9dba
Charger1256A <aditya.ram112005@gmail.com>
2022-01-03 23:17:54 -0800

    easter egg

--- static/src/components/Main.js
+++ static/src/components/Main.js
@@ -1,5 +1,5 @@
 import NavBar from "../navBar";
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import TextField from '@mui/material/TextField';
 import Box from '@mui/material/Box';
 import {Grid} from "@mui/material";
@@ -8,6 +8,46 @@ function Main(props) {
     const [text, setText] = useState('')
     const [helper, setHelper] = useState('')
     const [error, setError] = useState(false)
+    const [keys, setKeys] = useState([]);
+    const [egg, setEgg] = useState(false);
+
+    useEffect(() => {
+        var cmdDown = false;
+
+        document.body.addEventListener('keydown', function(event) {
+          var key = event.keyCode || event.charCode || 0;
+          if ([91,93,224,17].indexOf(key) !== -1) {
+            cmdDown = true;
+          }
+        //   console.log('CMD DOWN: ' + cmdDown.toString());    
+        });
+        
+        document.body.addEventListener('keyup', function(event) {
+          var key = event.keyCode || event.charCode || 0;
+          if ([91,93,224,17].indexOf(key) !== -1) {
+            cmdDown = false;
+          }
+        //   console.log('CMD DOWN: ' + cmdDown.toString());
+          var c = String.fromCharCode(key);
+          var lkeys = keys;
+          if (lkeys.length > 12) {
+              lkeys.shift();
+          }
+          lkeys.push(c);
+          setKeys(lkeys)
+        //   console.log(lkeys);
+          if (arraysEqual(lkeys, ['M', 'O', 'N', 'K', 'E', 'S', 'A', 'R', 'E', 'G', 'O', 'A', 'T'])) {
+              setEgg(!egg);
+              setKeys([]);
+          }
+        });
+    }, [])
+
+    function arraysEqual(a1,a2) {
+        /* WARNING: arrays must not contain {objects} or behavior may be undefined */
+        console.log(a1)
+        return JSON.stringify(a1)==JSON.stringify(a2);
+    }
 
     const isValid = (t) => {
         let valid = /^\d+$/.test(t);
@@ -30,7 +70,7 @@ function Main(props) {
         if (event.key === 'Enter') {
             if (isValid(text)) {
                 if (text === "OGMonkeLARA") {
-                    console.log("monke")
+                    // console.log("monke")
                     props.setCoolKid(true);
                     setText('')
                 } else {
@@ -65,30 +105,57 @@ function Main(props) {
 
     return (
         <React.Fragment>
-            <div className="container">
-            <Grid
-                component="TextField"
-                sx={{
-                    '& .MuiTextField-root': { m: 1, width: '25ch' },
-                }}
-                noValidate
-                autoComplete="off"
-                onKeyUp={handleKeyDown}
-                alignItems="center"
-                alignContent="center"
-                justifyContent="center"
-                container
-            >
-                <TextField
-                    id="login-box"
-                    label="Student ID"
-                    variant="standard"
-                    error={error}
-                    value={text}
-                    helperText={helper}
-                    onChange={s => isValid(s.target.value)}/>
-            </Grid>
-            </div>
+            {egg ? (
+                            <div className="container" >
+                            <Grid
+                                component="TextField"
+                                sx={{
+                                    '& .MuiTextField-root': { m: 1, width: '25ch' },
+                                }}
+                                noValidate
+                                autoComplete="off"
+                                onKeyUp={handleKeyDown}
+                                alignItems="center"
+                                alignContent="center"
+                                justifyContent="center"
+                                container
+                            >
+                                <TextField
+                                    id="login-box"
+                                    label="Student ID"
+                                    variant="standard"
+                                    error={error}
+                                    value={text}
+                                    helperText={helper}
+                                    onChange={s => isValid(s.target.value)}/>
+                            </Grid>
+                            </div>
+            ) : (
+                <div>
+                <Grid
+                    component="TextField"
+                    sx={{
+                        '& .MuiTextField-root': { m: 1, width: '25ch' },
+                    }}
+                    noValidate
+                    autoComplete="off"
+                    onKeyUp={handleKeyDown}
+                    alignItems="center"
+                    alignContent="center"
+                    justifyContent="center"
+                    container
+                >
+                    <TextField
+                        id="login-box"
+                        label="Student ID"
+                        variant="standard"
+                        error={error}
+                        value={text}
+                        helperText={helper}
+                        onChange={s => isValid(s.target.value)}/>
+                </Grid>
+                </div>
+            )}
         </React.Fragment>
     );
 }
