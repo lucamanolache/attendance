65cce3cc50dbf20686de6960d1347e2d2bbd7e4b
Luca Manolache <luca1230@gmail.com>
2022-01-10 09:26:34 -0800

    Fixed incorrect paths

--- src/schema/student.rs
+++ src/schema/student.rs
@@ -6,7 +6,7 @@ pub struct Student {
     pub id: u32,
     pub name: String,
     pub valid_time: i64,
-    pub events: Vec<(DateTime<Local>, DateTime<Local>)>,
+    pub events: Vec<(DateTime<Local>, Option<DateTime<Local>>)>,
     pub login_status: Option<DateTime<Local>>,
     pub subteam: String,
     pub slack_id: String,
